affd338f52080c58efbb282631676abebaa21551
Nora Rusterholz <norarusterholz@gmail.com>
2015-06-06 21:38:53 +0200

    Star Rating, href to index.html after click on save or cancel, Style adapted to index.html

--- js/newNote.js
+++ js/newNote.js
@@ -1,3 +1,28 @@
-/**
- * Created by Nora on 01.06.15.
- */
+function starRating() {
+    for (i = 0; i < document.getElementsByName('rating').length; i++) {
+        if(document.getElementsByName('rating')[i].checked == true) {
+            return document.getElementsByName('rating')[i].value;
+        }
+    }
+
+}
+
+document.getElementById("saveButton").onclick = function() {
+    var note = {
+        title: document.getElementById("title").value,
+        description: document.getElementById("description").value,
+        finishDate: document.getElementById("finishDate").value,
+        importance:  starRating()
+
+    };
+
+    console.log(note);
+    console.log(JSON.stringify(note));
+
+    localStorage.setItem("notes", JSON.stringify(note));
+    location.href = "index.html";s
+};
+
+document.getElementById("cancelButton").onclick = function () {
+    location.href = "index.html";
+};
