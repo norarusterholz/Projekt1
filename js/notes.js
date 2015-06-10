77fb2a93c5af1b8febc8db8c04006974507f9829
Min Jeong Kim Kühne <minjeong.kimkuehne@tages-anzeiger.ch>
2015-06-10 15:56:09 +0200

    Added template/ Created notes.html from index.html/ Created helpers.css/ Renamed style.css as resets.css

--- js/notes.js
+++ js/notes.js
@@ -1,20 +1,31 @@
-var notes = [
-    {
-        "date": "Nächsten Mittwoch",
-        "title": "CAS FEE Selbststudium/ Projekt Aufgabe erledigen",
-        "status": "finished",
-        "content": "HTML für de note App erstelln CSS erstellen für die note App"
-    },
-    {
-        "date": "Heute",
-        "title": "Einkaufen",
-        "status": "finished",
-        "content": "Eier, Milch, Butter..."
-    },
-    {
-        "date": "Morgen",
-        "title": "Mama Anrufen",
-        "status": "Not yet",
-        "content": "078 616 2293"
-    }
-];
+var source   = $("#entry-template").html();
+var template = Handlebars.compile(source);
+
+var notes = localStorage.getItem("notes");
+notes = JSON.parse(notes);
+
+console.log();
+
+var data = {
+    notes: [
+        {
+            date: "Nächsten Mittwoch",
+            title: "CAS FEE Selbststudium/ Projekt Aufgabe erledigen",
+            status: "finished",
+            content: "HTML für de note App erstelln CSS erstellen für die note App"
+        },
+        {
+            date: "Heute",
+            title: "Einkaufen",
+            status: "finished",
+            content: "Eier, Milch, Butter..."
+        },
+        {
+            date: "Morgen",
+            title: "Mama Anrufen",
+            status: "Not yet",
+            content: "078 616 2293"
+        }
+    ]};
+
+$("#notes").html(template(data));
