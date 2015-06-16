2e71141afd14d23140fac3c600916e5772671fb7
Min-Jeong Kim <mimi@nordformat.ch>
2015-06-16 20:53:03 +0200

    Added Handlebars/ Added .sort() function

--- client/js/notes.js
+++ client/js/notes.js
@@ -1,7 +1,63 @@
 var source = $("#entry-template").html();
 var template = Handlebars.compile(source);
+var notes = JSON.parse(localStorage.getItem("notes"));
+var $notes = $("#notes");
 
-var notes = localStorage.getItem("notes");
-notes = JSON.parse(notes);
+var renderNotes = function() {
+	$notes.html(template(notes));
+};
 
-$("#notes").html(template(notes));
+Handlebars.registerHelper("for", function(stars, options) {
+	var html = "";
+
+	for (var i = 0; i < parseInt(stars); i++) {
+		html = html + options.fn();
+	}
+
+	return new Handlebars.SafeString(html);
+});
+
+$("#finishDateSort").click(function(){
+	notes.sort(function(a, b) {
+		return new Date(a.finishDate) > new Date(b.finishDate);
+	});
+
+	renderNotes();
+});
+
+$("#importanceSort").click(function(){
+	notes.sort(function(a, b) {
+		return a.importance > b.importance;
+	});
+
+	renderNotes();
+});
+
+$("#createdDateSort").click(function(){
+	notes.sort(function(a, b) {
+		return a.createdDate < b.createdDate;
+	});
+
+	renderNotes();
+});
+
+$("#finishedSort").click(function(){
+	notes.sort(function(a) {
+		return a.finished === true; 
+	});
+
+	renderNotes();
+});
+
+renderNotes();
+
+$(".finished").click(function(e){
+	$target = $(e.target);
+
+	var index = $target.closest(".note").index();
+
+	notes[index].finshed = $target.prop("checked");
+	console.log("checked"),
+
+	localStorage.setItem("notes", JSON.stringify(notes));
+});
